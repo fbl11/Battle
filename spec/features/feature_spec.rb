c8af9315213199732e8f8dc3b936f5f11f6a4124
fbl11 <freyab_london@yahoo.co.uk>
2018-09-19 16:05:18 +0100

    User Story 2: Hit Points (wip)

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -2,16 +2,28 @@
 
 # As two Players,
 # So we can play a personalised game of Battle,
-# We want to Start a fight by entering our names and seeing them
+# We want to Start a fight by entering our names and seeing the
 
-RSpec.feature "Enter player names", :type => :feature do
-  scenario 'Users submit their player names' do
+describe 'Features' do
+
+  before(:each) {
     visit('/')
-
-    fill_in "name_one", :with => "Astaroth"
-    fill_in "name_two", :with => "Nightmare"
+    fill_in "name_one", with: "Astaroth"
+    fill_in "name_two", with: "Nightmare"
     click_button "Enter Players"
+  }
 
-    expect(page).to have_text('The next battle will be between Astaroth and Nightmare')
+  feature "Enter player names" do
+    scenario 'Users submit their player names' do
+      expect(page).to have_text('The next battle will be between Astaroth and Nightmare')
+    end
+
   end
+
+  feature "See Player 2's hit points" do
+    scenario "Player one wants to compare their score to Player two's score" do
+      expect(page).to have_text('Nightmare: 100 HP')
+    end
+  end
+
 end
